8093df9b723aadcc7f4dc57b5400539f152b4517
1-d <jason_yuen2007@hotmail.com>
2016-11-07 14:08:59 -0500

    Add files via upload

--- SimpleGame/Game.java
+++ SimpleGame/Game.java
@@ -11,27 +11,59 @@ import java.io.IOException;
 public class Game extends World
 {
     
-    private final InputInterface userInput;
-    
     /**
      * Constructor for objects of class Game.
+     * This method is used for playing a.
      * 
      */
-    public Game()
+    public Game(String levelPath) throws IOException
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        // Create a new world with 512x512 cells with a cell size of 1x1 pixels.
+        super(512, 512, 1);
         userInput = new InputInterface();
     }
     
     /**
      * Constructor for objects of class Game.
-     * 
+     * This method is used for replaying a file.
      */
-    public Game(String replayPath) throws IOException
+    public Game(String levelPath, String replayPath) throws IOException
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        // Create a new world with 512x512 cells with a cell size of 1x1 pixels.
+        super(512, 512, 1);
         userInput = new InputInterface(replayPath); 
     }
+    
+    // user input methods
+    private final InputInterface userInput;
+    
+    public boolean keyRight()
+    {
+        return userInput.right;
+    }
+    
+    public boolean keyUp()
+    {
+        return userInput.up;
+    }
+    
+    public boolean keyLeft()
+    {
+        return userInput.left;
+    }
+    
+    public boolean keyDown()
+    {
+        return userInput.down;
+    }
+    
+    // user variables
+    Player player;
+    
+    public int getPlayerDist(int x, int y)
+    {
+        // simple distance formula
+        return (int) Math.sqrt(Math.pow(player.getX()-x, 2) + Math.pow(player.getY()-y, 2));
+    }
+    
 }
--- SimpleGame/Title.java
+++ SimpleGame/Title.java
@@ -15,7 +15,7 @@ public class Title extends World
      */
     public Title()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        // Create a new world with 512x512 cells with a cell size of 1x1 pixels.
+        super(512, 512, 1); 
     }
 }
