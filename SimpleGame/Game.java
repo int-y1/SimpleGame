cd7dd34fef881527e18d0485b4322a78e7af5a1f
1-d <jason_yuen2007@hotmail.com>
2016-12-02 14:42:54 -0500

    Add files via upload

--- SimpleGame/Game.java
+++ SimpleGame/Game.java
@@ -11,17 +11,16 @@ import java.io.IOException;
 /**
  * Write a description of class Game here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Jason Yuen
+ * @version a0.01
  */
 public class Game extends World
 {
     
     private final int LEVEL;
-    private Queue<Integer> instTime = new LinkedList<Integer>();
-    private Queue<Integer> instID = new LinkedList<Integer>();
-    private Queue<ArrayList<Integer> > instInfo = new LinkedList<ArrayList<Integer> >();
     
+    private final InputInterface USER_INPUT;
+    private final LevelReader LR;
     
     /**
      * Constructor for objects of class Game.
@@ -32,17 +31,26 @@ public class Game extends World
     {
         // screen size is 512x512 pixels
         super(512, 512, 1);
+        
+        // get the user's input interface
         USER_INPUT = new InputInterface(replayPath);
-        // get the level info
+        
+        // get the level file into a Scanner
         InputStream stream = getClass().getResourceAsStream(levelPath);
         Scanner sc = new Scanner(stream);
-        LEVEL = sc.nextInt();
-        int instructions = sc.nextInt();
+        
+        // get the first line
+        String temp = sc.nextLine();                    // first line from Scanner
+        String[] args = temp.trim().split("\\s+");      // separate the String by whitespace
+        
+        // parse the first line from args
+        LEVEL = Integer.parseInt(args[0]);
+        
+        // send Scanner to another class for processing
+        LR = new LevelReader(this, sc);
     }
     
     // user input methods
-    private final InputInterface USER_INPUT;
-    
     public boolean keyRight()
     {
         return USER_INPUT.right;
